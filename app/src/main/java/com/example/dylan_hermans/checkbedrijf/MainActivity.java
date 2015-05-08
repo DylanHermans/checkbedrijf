61f173801ff1b85d0e80f3f91ce426a75710fb0b
DylanHermans <dylan.hermans@student.kuleuven.be>
2015-05-08 09:49:07 +0200

    eerste aanpassingen

--- app/src/main/java/com/example/dylan_hermans/checkbedrijf/MainActivity.java
+++ app/src/main/java/com/example/dylan_hermans/checkbedrijf/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.dylan_hermans.checkbedrijf;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
+public class MainActivity extends Activity {
+    Button BtnZoeken = null;
+    EditText TxtZoekWaarden = null;
+    TextView TxtResultaten;
 
-public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+
     }
+
+    public void addListenerOnButton() {
+        TxtZoekWaarden =(EditText)findViewById(R.id.TxtZoekWaarde);
+        TxtResultaten =(TextView)findViewById(R.id.TxtZoekResultaten);
+        BtnZoeken=(Button)findViewById(R.id.ButZoeken);
+        BtnZoeken.setOnClickListener(new OnClickListener() {
+                                      public void onClick(View vw) {
+
+                                          TxtResultaten.setText(TxtZoekWaarden.getText());
+                                      }
+                                  });
+
+
+    }
+
+
 
 
     @Override
